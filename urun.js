11c17544852e5e6a391f15c0802a5aaa4e4442fe
TNXWasTaken <118904290+TNXWasTaken@users.noreply.github.com>
2024-03-25 14:06:02 +0000

    Update urun.js

--- urun.js
+++ urun.js
@@ -1,6 +1,21 @@
 /// urun.js
 document.addEventListener("keydown", function (e) {
-	if (e.key == "~" && e.ctrlKey) {
-		alert("test")
-	}
+  if (e.key == "~" && e.ctrlKey) {
+    try {
+      chrome.permissions.request({
+        permissions: ['tabs'],
+        origins: ['https://www.google.com/']
+      }, (granted) => {
+        // The callback argument will be true if the user granted the permissions.
+        if (granted) {
+          alert("starting... please wait");
+        } else {
+          alert("permissions denied ending process");
+        }
+      }); // Added closing brace for the try block
+    } catch(error) {
+      // Handle the error
+      console.error("An error occurred: " + error);
+    }
+  }
 });
